f7c9fa03562cf6c8faf040f90a61512214266e0a
tkmusic <68860474+tkmusic@users.noreply.github.com>
2022-10-19 14:33:27 +0200

    Finished the Basic Scanner

--- src/com/tonikrug/jpulse/Scanner.java
+++ src/com/tonikrug/jpulse/Scanner.java
@@ -11,8 +11,30 @@ class Scanner {
     private int line = 1;
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
+
     Scanner(String source) {
         this.source = source;
+    }
+
+    private static final Map<String, TokenType> keywords;
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",AND);
+        keywords.put("class",CLASS);
+        keywords.put("else",ELSE);
+        keywords.put("false",FALSE);
+        keywords.put("for",FOR);
+        keywords.put("fun",FUN);
+        keywords.put("if",IF);
+        keywords.put("nil",NIL);
+        keywords.put("or",OR);
+        keywords.put("print",PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",SUPER);
+        keywords.put("this",THIS);
+        keywords.put("true",TRUE);
+        keywords.put("var",VAR);
+        keywords.put("while",WHILE);
     }
 
     List<Token> scanTokens() {
@@ -63,11 +85,21 @@ class Scanner {
             default:
                 if (isDigit(c)) {
                     number();
+                } else if (isAlpha(c)) {
+                    identifier();
                 } else {
                     JPulse.error(line, "Unexpected character.");
             }
             break;
         }
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+        addToken(type);
     }
 
     private void number() {
@@ -114,6 +146,15 @@ class Scanner {
         return source.charAt(current + 1);
     }
 
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
 
     private boolean isDigit(char c) {
         return c >= '0' && c <= '9';
